a8baffd6c5d14e214eeed114e447f67287f6deca
Kasil <kasil@loutre.info>
2022-09-28 18:59:07 +0200

    Add preliminary support for keyboard

--- src/input.rs
+++ src/input.rs
@@ -1,10 +1,136 @@
-use embedded_hal_02::digital::v2::InputPin;
+use core::{default, mem::size_of};
+
+use embedded_hal_02::digital::v2::{InputPin, OutputPin};
+use radio_sx127x::base::Hal;
 
 pub struct Button<P>
 where
     P: InputPin,
 {
     button: P,
+}
+
+#[derive(Default)]
+pub struct Modifier {
+    pub star: bool,
+    pub shift_l: bool,
+    pub shift_r: bool,
+    pub dollar: bool,
+    pub sharp: bool,
+}
+
+pub fn extract_modifiers(data: u32) -> (Modifier, u32) {
+    let mut modifier = Modifier::default();
+
+    if (data & 0x80000000) != 0 {
+        modifier.star = true;
+    }
+    if (data & 0x00000080) != 0 {
+        modifier.shift_l = true;
+    }
+    if (data & 0x01000000) != 0 {
+        modifier.shift_r = true;
+    }
+    if (data & 0x00000100) != 0 {
+        modifier.dollar = true;
+    }
+    if (data & 0x00800000) != 0 {
+        modifier.sharp = true;
+    }
+    (modifier, data & 0x7E7FFE7F)
+}
+
+pub const KEYS_ALPHA: [char; 32] = [
+    'q', 'w', 'e', 'r', 'd', 's', 'a', '^', '$', 'z', 'x', 'c', 'v', 'f', 't', 'y', 'g', 'u', 'h',
+    'b', 'n', 'm', '_', '#', '%', 'l', 'k', 'j', 'i', 'o', 'p', '*',
+];
+pub const KEYS_CAPS: [char; 32] = [
+    'Q', 'W', 'E', 'R', 'D', 'S', 'A', '^', '$', 'Z', 'X', 'C', 'V', 'F', 'T', 'Y', 'G', 'U', 'H',
+    'B', 'N', 'M', '_', '#', '%', 'L', 'K', 'J', 'I', 'O', 'P', '*',
+];
+pub const KEYS_NUM: [char; 32] = [
+    '1', '2', '3', '4', 'd', 's', 'a', '^', '$', 'z', 'x', 'c', 'v', 'f', '5', '6', 'g', '7', 'h',
+    'b', 'n', 'm', '_', '#', '%', 'l', 'k', 'j', '8', '9', '0', '*',
+];
+
+pub fn get_one_char_from(data: u32, source: &[char; 32]) -> Option<char> {
+    if data.count_ones() == 1 {
+        Some(source[data.trailing_zeros() as usize])
+    } else {
+        None
+    }
+}
+
+pub fn get_one_char(data: u32) -> Option<char> {
+    get_one_char_from(data, &KEYS_ALPHA)
+}
+
+pub struct ShiftRegister<CLK, DATA, LATCH, VAL>
+where
+    CLK: OutputPin,
+    DATA: InputPin,
+    LATCH: OutputPin,
+{
+    clk: CLK,
+    data: DATA,
+    latch: LATCH,
+    phantom_data: core::marker::PhantomData<VAL>,
+}
+
+pub trait ReadRegister<VAL> {
+    fn read(&mut self) -> VAL;
+}
+impl<CLK, DATA, LATCH, VAL> ShiftRegister<CLK, DATA, LATCH, VAL>
+where
+    CLK: OutputPin,
+    DATA: InputPin,
+    LATCH: OutputPin,
+    DATA::Error: core::fmt::Debug,
+    VAL: core::ops::ShlAssign + Default + core::ops::AddAssign + From<bool> + Copy,
+{
+    pub fn new(clk: CLK, data: DATA, latch: LATCH) -> Self {
+        Self {
+            clk,
+            data,
+            latch,
+            phantom_data: core::marker::PhantomData::default(),
+        }
+    }
+}
+impl<CLK, DATA, LATCH, VAL> ReadRegister<VAL> for ShiftRegister<CLK, DATA, LATCH, VAL>
+where
+    CLK: OutputPin,
+    DATA: InputPin,
+    LATCH: OutputPin,
+    DATA::Error: core::fmt::Debug,
+    VAL: core::ops::ShlAssign + Default + core::ops::AddAssign + From<bool> + Copy,
+{
+    fn read(&mut self) -> VAL {
+        let mut v = VAL::default();
+        let one: VAL = true.into();
+
+        let bits = size_of::<VAL>() * 8;
+
+        self.clk.set_low();
+        self.latch.set_low();
+        cortex_m::asm::delay(10);
+        self.latch.set_high();
+
+        for _ in 0..bits {
+            v <<= one;
+            cortex_m::asm::delay(10);
+
+            if self.data.is_high().unwrap() {
+                v += one;
+            }
+            self.clk.set_high();
+            cortex_m::asm::delay(10);
+
+            self.clk.set_low();
+        }
+
+        v
+    }
 }
 
 impl<P> Button<P>
---
+++ src/keyboard.rs
@@ -0,0 +1,246 @@
+#![no_std]
+#![no_main]
+mod blink;
+mod input;
+mod stuff;
+
+use bsp::{entry, hal::gpio::FunctionSpi};
+use embedded_graphics::text::renderer::TextRenderer;
+use heapless::String;
+use input::*;
+use stuff::*;
+
+use defmt::*;
+use defmt_rtt as _;
+use embedded_hal_compat::eh0_2::digital::v2::{InputPin, OutputPin};
+use embedded_hal_compat::eh0_2::spi::{Mode, Phase, Polarity, MODE_0};
+use embedded_hal_compat::eh1_0::spi::blocking::{Transactional, TransferInplace, Write};
+use embedded_hal_compat::{ForwardCompat, ReverseCompat};
+use fugit::RateExtU32;
+use numtoa::NumToA;
+use panic_probe as _;
+
+use display_interface_spi::SPIInterface;
+use embedded_graphics::{
+    draw_target::DrawTarget,
+    mono_font::{ascii::FONT_6X10, MonoTextStyle},
+    pixelcolor::Rgb565,
+    prelude::*,
+    text::*,
+};
+
+use mipidsi::*;
+// Provide an alias for our BSP so we can switch targets quickly.
+// Uncomment the BSP you included in Cargo.toml, the rest of the code does not need to change.
+use rp_pico as bsp;
+// use sparkfun_pro_micro_rp2040 as bsp;
+
+use bsp::hal::{
+    clocks::{init_clocks_and_plls, Clock},
+    pac,
+    sio::Sio,
+    spi::Spi,
+    watchdog::Watchdog,
+};
+
+//use hal::spidev::{self, SpidevOptions};
+//use hal::sysfs_gpio::Direction;
+//use bsp::hal::Delay;
+//use hal::{Pin, Spidev};
+
+use radio_sx127x::prelude::*;
+use radio_sx127x::Error as sx127xError; // Error name conflict with hals
+
+use radio::{Receive, Transmit};
+
+use crate::input::Button2;
+
+struct Disp<D, S>
+where
+    //    DI: display_interface::WriteOnlyDataCommand,
+    //    RST: OutputPin,
+    //    MODEL: mipidsi::models::Model,
+    D: DrawTarget,
+    S: embedded_graphics::text::renderer::TextRenderer,
+{
+    display: D, //Display<DI, RST, MODEL>,
+    cursor: i32,
+    style: S,
+}
+
+#[entry]
+fn main() -> ! {
+    info!("Program start");
+    let mut pac = pac::Peripherals::take().unwrap();
+    let core = pac::CorePeripherals::take().unwrap();
+    let mut watchdog = Watchdog::new(pac.WATCHDOG);
+    let sio = Sio::new(pac.SIO);
+
+    // External high-speed crystal on the pico board is 12Mhz
+    let external_xtal_freq_hz = 12_000_000u32;
+    let clocks = init_clocks_and_plls(
+        external_xtal_freq_hz,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .ok()
+    .unwrap();
+
+    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+
+    let pins = bsp::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
+    let mut _led = pins.led.into_push_pull_output();
+    //blink(&mut led, &"Hello!");
+    let _mosi_display = pins.gpio19.into_mode::<FunctionSpi>();
+    let _sck_display = pins.gpio18.into_mode::<FunctionSpi>();
+    //    let cs_display = pins.gpio17.into_push_pull_output();
+    //    let dc_display = pins.gpio16.into_push_pull_output();
+
+    let spi_display = Spi::<_, _, 8>::new(pac.SPI0).init(
+        &mut pac.RESETS,
+        clocks.peripheral_clock.freq(),
+        65.MHz(),
+        &embedded_hal_compat::eh0_2::spi::MODE_3,
+    );
+    // create a DisplayInterface from SPI and DC pin, with no manual CS control
+    //let di = SPIInterface::new(spi_display, dc_display, cs_display);
+    // create the ILI9486 display driver in rgb666 color mode from the display interface and RST pin
+    //let mut display = Display::st7789(di, NoPin::default());
+    //display.init(&mut delay, DisplayOptions::default()).unwrap();
+    // clear the display to black
+    //display.clear(Rgb565::BLUE).unwrap();
+    //let style = MonoTextStyle::new(&FONT_6X10, Rgb565::WHITE);
+
+    //let mut led_pin = pins.led.into_push_pull_output();
+
+    let _miso = pins.gpio8.into_mode::<FunctionSpi>();
+    let _mosi = pins.gpio11.into_mode::<FunctionSpi>();
+    let _clk = pins.gpio10.into_mode::<FunctionSpi>();
+
+    let spi = Spi::<_, _, 8>::new(pac.SPI1);
+    let spi = spi
+        .init(
+            &mut pac.RESETS,
+            clocks.peripheral_clock.freq(),
+            20.MHz(),
+            &embedded_hal_compat::eh0_2::spi::MODE_0,
+        )
+        .forward();
+
+    //  let mut lora = sx127x_lora::LoRa::new(spi, cs, reset, FREQUENCY, delay)
+    //    .expect("Failed to communicate with radio module!");
+
+    //lora.set_tx_power(17, 1); //Using PA_BOOST. See your board for correct pin.
+
+    let mut pull_up = pins.gpio17.into_push_pull_output();
+    let k_clk = pins.gpio15.into_push_pull_output();
+    let k_data = pins.gpio16.into_floating_input();
+    let k_latch = pins.gpio14.into_push_pull_output();
+    pull_up.set_high();
+
+    let mut keyboard: ShiftRegister<_, _, _, u32> = ShiftRegister::new(k_clk, k_data, k_latch);
+    let mut state = State::Init;
+    let mut str: String<128> = String::new();
+    let mut last = 0u32;
+    let mut ready = true;
+    loop {
+        let key = keyboard.read();
+        if key != last {
+            let (m, c) = extract_modifiers(key);
+            let default = get_one_char(c);
+
+            let modified = get_one_char_from(
+                c,
+                if m.shift_l || m.shift_r {
+                    &KEYS_CAPS
+                } else if m.dollar {
+                    &KEYS_NUM
+                } else {
+                    &KEYS_ALPHA
+                },
+            );
+            if modified.is_none() {
+                ready = true;
+            }
+            if let (Some(car), true) = (modified, ready) {
+                _ = str.push(car);
+                info!("{}", str.as_str());
+                ready = false;
+            }
+
+            if ready && m.sharp {
+                info!("SENDING {}", str.as_str());
+                str.clear();
+            }
+            /*info!(
+                "key is {:08X},{}{}{}{}{} {}    {}",
+                key,
+                if m.star { '*' } else { ' ' },
+                if m.shift_l { '^' } else { ' ' },
+                if m.shift_r { '%' } else { ' ' },
+                if m.dollar { '$' } else { ' ' },
+                if m.sharp { '#' } else { ' ' },
+                default,
+                modified
+            )*/
+        }
+        last = key;
+    }
+}
+
+use core::fmt::Debug;
+use core::ptr::read;
+
+impl State {
+    fn run_state<Hal: radio_sx127x::base::Hal, K: ReadRegister<u32>, T: Debug + 'static, D, S>(
+        &self,
+        //lora: &mut radio_sx127x::Sx127x<Hal>,
+        keyboard: &mut K,
+        disp: &mut Disp<D, S>,
+    ) -> Result<Self, stuff::Error<T>>
+    where
+        stuff::Error<T>: From<sx127xError<T>>,
+        D::Error: Debug,
+        stuff::Error<T>: From<radio_sx127x::Error<<Hal as radio_sx127x::base::Hal>::Error>>,
+        //        DI: display_interface::WriteOnlyDataCommand,
+        //        RST: OutputPin,
+        //        MODEL: mipidsi::models::Model,
+        D: DrawTarget<Color = <S as TextRenderer>::Color>,
+        S: embedded_graphics::text::renderer::TextRenderer + Copy,
+    {
+        match self {
+            State::Init => {
+                info!("init");
+                Ok(State::PrepareIdle)
+            }
+            State::Reset => {
+                crate::panic!("reset unimplemented")
+            }
+            State::PrepareIdle => {
+                info!("to idle");
+                //lora.start_receive()?;
+
+                Ok(State::Idle)
+            }
+            State::Idle => Ok(State::Idle),
+            State::Sending => Ok(State::Idle),
+            State::Received => Ok(State::PrepareIdle),
+            State::SendingDone => {
+                //lora.start_receive()?;
+                Ok(Self::Idle)
+            }
+            State::Panic => {
+                crate::panic!("panic")
+            }
+        }
+    }
+}
